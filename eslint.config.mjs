2e7426e1781b150ac51ff6712ce5a5df62ebe9af
betogroo <luizhumberto@gmail.com>
2025-11-03 09:18:20 -0300

    chore: configure prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,22 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import prettierPlugin from 'eslint-plugin-prettier'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  plugins: {
+    prettier: prettierPlugin,
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'all',
+        printWidth: 100,
+        bracketSameLine: false,
+        vueIndentScriptAndStyle: true,
+      },
+    ],
+  },
+})
