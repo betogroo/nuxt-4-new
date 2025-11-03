5206c55606be626e5994e60c674aec106ec37205
betogroo <luizhumberto@gmail.com>
2025-11-03 09:20:28 -0300

    chore: add vue eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,5 +18,41 @@ export default withNuxt({
         vueIndentScriptAndStyle: true,
       },
     ],
+    // suas regras Vue / TS existentes
+    'vue/valid-v-slot': ['error', { allowModifiers: true }],
+    'vue/block-order': ['error', { order: ['script[setup]', 'template', 'style'] }],
+    'vue/define-macros-order': [
+      'error',
+      { order: ['defineOptions', 'defineProps', 'defineEmits'] },
+    ],
+    'vue/attributes-order': [
+      'error',
+      {
+        order: [
+          'DEFINITION',
+          'LIST_RENDERING',
+          'CONDITIONALS',
+          'RENDER_MODIFIERS',
+          'GLOBAL',
+          ['UNIQUE', 'SLOT'],
+          'TWO_WAY_BINDING',
+          'OTHER_DIRECTIVES',
+          'OTHER_ATTR',
+          'EVENTS',
+          'CONTENT',
+        ],
+        alphabetical: true,
+      },
+    ],
+    'vue/max-attributes-per-line': 'off',
+    /*  'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: { max: 1 },
+        multiline: { max: 1 },
+      },
+    ], */
+    'vue/no-multiple-template-root': 'off',
+    'vue/multi-word-component-names': 'off',
   },
 })
