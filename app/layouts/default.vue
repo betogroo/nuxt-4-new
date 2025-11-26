2dffbb0c3b83d7be5cf608be6bf9aa1f5f55b15a
betogroo <luizhumberto@gmail.com>
2025-11-26 08:46:36 -0300

    refactor: optimize nav bar

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,14 +1,9 @@
 <template>
   <div>
-    <nav>
-      <ul>
-        <nuxt-link to="/">
-          <li>Home</li>
-        </nuxt-link>
-        <nuxt-link to="/about">
-          <li>About</li>
-        </nuxt-link>
-      </ul>
+    <nav class="d-flex justify-end ga-2 mr-12">
+      <nuxt-link to="/">Home</nuxt-link>
+      <div>|</div>
+      <nuxt-link to="/about">About</nuxt-link>
     </nav>
     <slot />
   </div>
