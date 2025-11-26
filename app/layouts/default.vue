7ea3709dd2bb1b96cceaad1213e6e0967feec708
betogroo <luizhumberto@gmail.com>
2025-11-26 08:51:46 -0300

    refactor: optimize app structure

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,10 +1,14 @@
 <template>
-  <div>
+  <v-app>
     <nav class="d-flex justify-end ga-2 mr-12">
       <nuxt-link to="/">Home</nuxt-link>
       <div>|</div>
       <nuxt-link to="/about">About</nuxt-link>
     </nav>
-    <slot />
-  </div>
+    <v-main>
+      <v-container>
+        <slot />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
