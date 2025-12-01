d08a1087982f9ef0a07238118acd5e59cfffebc5
betogroo <luizhumberto@gmail.com>
2025-12-01 13:38:26 -0300

    feat: add page meta title on created pages

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+  definePageMeta({
+    title: 'Sobre',
+  })
+</script>
 <template>
   <div><h1>About</h1></div>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+  definePageMeta({
+    title: 'Home Page',
+  })
+</script>
 <template>
   <div><h1>Home Page</h1></div>
 </template>
