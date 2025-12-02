620416c9bdaa5dd704083497282d165e2169d6c6
betogroo <luizhumberto@gmail.com>
2025-12-02 10:25:09 -0300

    fix: change schema import/export order

--- app/schemas/index.ts
+++ app/schemas/index.ts
@@ -1,5 +1,5 @@
 export * as z from 'zod'
 
 export * from './app/icon-button.schema'
-export * from './app/page-meta.schema'
 export * from './app/menu.schema'
+export * from './app/page-meta.schema'
