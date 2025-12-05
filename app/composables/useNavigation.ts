579b2127d309c540e71d8064df556f5f5e92cd34
betogroo <luizhumberto@gmail.com>
2025-12-05 16:01:38 -0300

    feat: add all routes computed

--- app/composables/useNavigation.ts
+++ app/composables/useNavigation.ts
@@ -1,5 +1,26 @@
+import { PageMetaSchema } from '~/schemas'
+
 const useNavigation = () => {
-  return {}
+  const router = useRouter()
+
+  const allRoutes = computed(() => {
+    return router
+      .getRoutes()
+      .filter((route) => route.meta.menu && route.meta.menu.title)
+      .map((route) => {
+        const parsed = PageMetaSchema.safeParse(route.meta)
+
+        if (!parsed.success) return null
+
+        return {
+          name: route.name,
+          path: route.path,
+          ...parsed.data,
+        }
+      })
+  })
+
+  return { allRoutes }
 }
 
 export default useNavigation
