7e5aee396654dccb23c520c7ba0374127887ab7e
betogroo <luizhumberto@gmail.com>
2025-12-05 17:01:54 -0300

    use get menu bu area on nav bar

--- app/components/app/nav-bar.vue
+++ app/components/app/nav-bar.vue
@@ -1,40 +1,20 @@
 <script setup lang="ts">
-  import type { IconButton } from '~/types'
-  import { IconButtonSchemaList, z } from '~/schemas'
-  const appBarItems: IconButton[] = [
-    {
-      name: 'Home',
-      to: '/',
-      icon: 'mdi-home',
-    },
-    {
-      name: 'About',
-      to: '/about',
-      icon: 'mdi-information',
-    },
-  ]
-
-  try {
-    IconButtonSchemaList.parse(appBarItems)
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      console.log(error)
-    }
-  }
+  const { getMenuByArea } = useNavigation()
+  const navbarItems = getMenuByArea('navbar')
 </script>
 
 <template>
   <v-app-bar color="primary" density="compact">
     <v-app-bar-title>Nuxt App</v-app-bar-title>
-    <div class="d-flex ga-2 mr-3">
+    <div class="d-flex ga- mr-3">
       <v-btn
-        v-for="item in appBarItems"
-        :key="item.name"
-        :icon="item.icon"
+        v-for="item in navbarItems"
+        :key="item?.path"
         :ripple="false"
-        size="24"
-        :to="item.to"
+        :to="item?.path"
         variant="plain"
-      /></div
-  ></v-app-bar>
+        >{{ item?.menu?.title }}</v-btn
+      >
+    </div></v-app-bar
+  >
 </template>
