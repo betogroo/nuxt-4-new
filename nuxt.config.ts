0fe19ca4c4b95d6a602ea77f8f7831612fd2cbf8
betogroo <luizhumberto@gmail.com>
2025-12-11 14:09:35 -0300

    chore: install pws plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/test-utils', 'vuetify-nuxt-module'],
+  modules: ['@nuxt/eslint', '@nuxt/test-utils', 'vuetify-nuxt-module', '@vite-pwa/nuxt'],
 })
