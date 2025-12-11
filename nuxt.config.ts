43a1235a86fb273cd000f38d38dc9167380d44f2
betogroo <luizhumberto@gmail.com>
2025-12-11 14:46:46 -0300

    chore: configure pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,42 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/test-utils', 'vuetify-nuxt-module', '@vite-pwa/nuxt'],
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Nuxt App',
+      short_name: 'App',
+      description: 'Nuxt App Nuxt 4 PWA',
+      theme_color: '#ffffff',
+      background_color: '#ffffff',
+      display: 'standalone',
+      start_url: '/',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-maskable.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+      ],
+    },
+    workbox: {
+      navigateFallback: '/', // fallback offline
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+    },
+    devOptions: {
+      enabled: true,
+      type: 'module',
+    },
+  },
 })
