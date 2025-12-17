f01061f65297463e7485413bb62a186a31d1d7d4
betogroo <luizhumberto@gmail.com>
2025-12-17 16:19:40 -0300

    refactor: login page do no use vuetify components

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -6,10 +6,29 @@
       hidden: true,
     },
   })
+
+  const email = ref<string>()
+  const password = ref<string>()
+
+  const testForm = () => {
+    console.log(email.value, password.value)
+  }
 </script>
 
 <template>
   <app-public-container>
-    <v-sheet color="red" height="300" rounded width="300">lll</v-sheet>
+    <ui-card-auth>
+      <ui-base-title>Login</ui-base-title>
+      <ui-base-form @submit="testForm">
+        <ui-form-stack>
+          <ui-form-text-field v-model="email" label="Email" type="email" />
+          <ui-form-text-field-password v-model="password" label="Senha" />
+          <ui-form-btn-submit>Submit</ui-form-btn-submit>
+        </ui-form-stack>
+      </ui-base-form>
+      <ui-card-footer>
+        Clique <nuxt-link to="/signup">aqui</nuxt-link> para se cadastrar
+      </ui-card-footer>
+    </ui-card-auth>
   </app-public-container>
 </template>
