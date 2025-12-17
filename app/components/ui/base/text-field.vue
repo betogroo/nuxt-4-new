fdfca0360f90376a421ebb582419a51d791b1586
betogroo <luizhumberto@gmail.com>
2025-12-17 16:11:38 -0300

    feat: base ui components created

---
+++ app/components/ui/base/text-field.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+  import type { TextFieldType } from '~/types'
+
+  interface Props {
+    label?: string
+    placeholder?: string
+    type?: TextFieldType
+    disabled?: boolean
+    loading?: boolean
+    error?: string
+    clearable?: boolean
+  }
+  defineProps<Props>()
+
+  const modelValue = defineModel<string | number | null>()
+</script>
+
+<template>
+  <v-text-field
+    v-model="modelValue"
+    :clearable="clearable"
+    :disabled="disabled"
+    :error="!!error"
+    :error-messages="error"
+    :label="label"
+    :loading="loading"
+    :placeholder="placeholder"
+    :type="type"
+    v-bind="$attrs"
+  />
+</template>
---
+++ app/components/ui/base/title.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1 class="text-h4 my-2"><slot /></h1>
+</template>
