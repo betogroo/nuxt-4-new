7406f737f94a3ff9b3ff556227cd63bdf2448f18
betogroo <luizhumberto@gmail.com>
2025-12-17 19:26:28 -0300

    feat: add validate on login form

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,4 +1,9 @@
 <script setup lang="ts">
+  import { useForm, useField } from 'vee-validate'
+  import { toTypedSchema } from '@vee-validate/zod'
+  import { LoginSchema } from '~/schemas'
+  import type { Login } from '~/types'
+
   definePageMeta({
     layout: 'no-nav',
     menu: {
@@ -7,23 +12,35 @@
     },
   })
 
-  const email = ref<string>()
-  const password = ref<string>()
+  const { values, handleSubmit, meta } = useForm<Login>({
+    validationSchema: toTypedSchema(LoginSchema),
+    initialValues: {
+      email: '',
+      password: '',
+    },
+  })
 
-  const testForm = () => {
-    console.log(email.value, password.value)
-  }
+  const color = computed(() => {
+    if (!meta.value.dirty) return ''
+    return !meta.value.valid ? 'error' : 'success'
+  })
+  const { value: email, errorMessage: emailError } = useField<string>('email')
+  const { value: password, errorMessage: passwordError } = useField<string>('password')
+
+  const onSubmit = handleSubmit(async () => {
+    console.log('LOGIN: ', values)
+  })
 </script>
 
 <template>
   <app-public-container>
     <ui-card-auth>
       <ui-base-title>Login</ui-base-title>
-      <ui-base-form @submit="testForm">
+      <ui-base-form @submit="onSubmit">
         <ui-form-stack>
-          <ui-form-text-field v-model="email" label="Email" type="email" />
-          <ui-form-text-field-password v-model="password" label="Senha" />
-          <ui-form-btn-submit>Submit</ui-form-btn-submit>
+          <ui-form-text-field v-model="email" :error="emailError" label="Email" type="email" />
+          <ui-form-text-field-password v-model="password" :error="passwordError" label="Senha" />
+          <ui-form-btn-submit :color="color" :disabled="!meta.valid">Login</ui-form-btn-submit>
         </ui-form-stack>
       </ui-base-form>
       <ui-card-footer>
