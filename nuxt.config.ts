4feadc07de386a702f043d63e2e21464bae9f0bc
betogroo <luizhumberto@gmail.com>
2025-12-18 08:40:51 -0300

    chore: change to vee-validate/nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/test-utils', 'vuetify-nuxt-module', '@vite-pwa/nuxt'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/test-utils',
+    'vuetify-nuxt-module',
+    '@vite-pwa/nuxt',
+    '@vee-validate/nuxt',
+  ],
   pwa: {
     devOptions: {
       enabled: true,
