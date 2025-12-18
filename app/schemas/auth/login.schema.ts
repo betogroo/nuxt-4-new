54e55c82da6f0cba9940d696900a8aefcc5f8686
betogroo <luizhumberto@gmail.com>
2025-12-18 08:39:01 -0300

    fix: zod 3 compatibility

--- app/schemas/auth/login.schema.ts
+++ app/schemas/auth/login.schema.ts
@@ -1,6 +1,6 @@
 import { z } from '~/schemas'
 
 export const LoginSchema = z.object({
-  email: z.email('E-mail inválido'),
+  email: z.string().email('Email inválido'),
   password: z.string().min(6, 'A senha deve ter no mínimo 6 caracteres'),
 })
