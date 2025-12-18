3d2b856325967c0741815a5665256bc33b7eef5a
betogroo <luizhumberto@gmail.com>
2025-12-18 16:27:54 -0300

    feat: create signup page

---
+++ app/pages/signup.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+  import { SignupSchema } from '~/schemas'
+  import type { Signup } from '~/types'
+
+  definePageMeta({
+    layout: 'no-nav',
+    menu: {
+      title: 'Signup',
+      hidden: true,
+    },
+  })
+
+  const { values, handleSubmit, meta } = useForm<Signup>({
+    validationSchema: toTypedSchema(SignupSchema),
+    initialValues: {
+      email: '',
+      password: '',
+      passwordConfirm: '',
+    },
+  })
+
+  const color = computed(() => {
+    if (!meta.value.dirty) return ''
+    return !meta.value.valid ? 'error' : 'success'
+  })
+
+  const onSubmit = handleSubmit(async () => {
+    const { passwordConfirm, ...payload } = values
+
+    console.log('SIGNUP: ', payload)
+  })
+</script>
+
+<template>
+  <app-public-container>
+    <ui-card-auth>
+      <ui-base-title>Signup</ui-base-title>
+      <ui-base-form @submit="onSubmit">
+        <ui-form-stack>
+          <ui-form-text-field label="Email" name="email" type="email" />
+          <ui-form-text-field-password label="Senha" name="password" />
+          <ui-form-text-field-password label="Confirme a senha" name="passwordConfirm" />
+          <ui-form-btn-submit :color="color" :disabled="!meta.valid">Login</ui-form-btn-submit>
+        </ui-form-stack>
+      </ui-base-form>
+      <ui-card-footer>
+        Clique <nuxt-link to="/login">aqui</nuxt-link> acessar o sistema
+      </ui-card-footer>
+    </ui-card-auth>
+  </app-public-container>
+</template>
