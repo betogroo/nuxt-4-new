c98f494d4911959ab550be9fae90311d379b9cdd
betogroo <luizhumberto@gmail.com>
2025-12-18 16:27:29 -0300

    chore: creat passwordConfirm type and schema

--- app/schemas/auth/login.schema.ts
+++ app/schemas/auth/login.schema.ts
@@ -4,3 +4,13 @@ export const LoginSchema = z.object({
   email: z.string().email('Email inválido'),
   password: z.string().min(6, 'A senha deve ter no mínimo 6 caracteres'),
 })
+
+export const SignupSchema = z
+  .object({
+    passwordConfirm: z.string().min(6, 'A confirmação deve ter no mínimo 6 dígitos'),
+  })
+  .merge(LoginSchema)
+  .refine((data) => data.password === data.passwordConfirm, {
+    message: 'As senhas devem ser idênticas',
+    path: ['passwordConfirm'],
+  })
--- app/types/auth/login.ts
+++ app/types/auth/login.ts
@@ -1,3 +1,4 @@
-import type { z, LoginSchema } from '~/schemas'
+import type { z, LoginSchema, SignupSchema } from '~/schemas'
 
 export type Login = z.infer<typeof LoginSchema>
+export type Signup = z.infer<typeof SignupSchema>
