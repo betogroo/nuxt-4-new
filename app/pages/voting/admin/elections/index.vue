798927056a4da677edcaae6ca38aee42dc2c05f2
betogroo <luizhumberto@gmail.com>
2025-12-22 16:56:57 -0300

    feat(wip): elections page

---
+++ app/pages/voting/admin/elections/index.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+  definePageMeta({
+    layout: 'default',
+    menu: {
+      title: 'Admin - Elections',
+      hidden: false,
+    },
+  })
+
+  const mockElection = {
+    id: 'c82f9c77-6bb2-4db1-bc73-2cfa9120f781',
+    name: 'High School Representative Election',
+    votingDate: '2025-08-12T14:00:00.000Z',
+    candidateDigits: 2,
+    createdAt: '2025-02-18T11:25:40.000Z',
+  }
+  const electionsStore = useElectionsStore()
+  const { elections } = storeToRefs(electionsStore)
+
+  const addElection = () => {
+    electionsStore.addElection(mockElection)
+    console.log(mockElection)
+  }
+</script>
+
+<template>
+  <div>
+    <ui-base-title>Eleições</ui-base-title>
+    <div>
+      <ui-form-btn-action @click="addElection">Nova Eleição</ui-form-btn-action>
+      <ui-form-btn-action color="red" @click="electionsStore.$reset">Reset</ui-form-btn-action>
+    </div>
+    <div>
+      <div v-if="!elections.length">Ainda não tem eleições cadastradas</div>
+      <div v-else class="d-flex flex-wrap justify-space-around">
+        <ui-base-card
+          v-for="item in elections"
+          :key="item.id"
+          class="ma-2 pa-2"
+          height="150"
+          :subtitle="item.votingDate"
+          :title="item.name"
+          width="400"
+        >
+          <div class="text-right">
+            <ui-form-btn-action color="green" variant="text">Editar</ui-form-btn-action>
+          </div>
+        </ui-base-card>
+      </div>
+    </div>
+  </div>
+</template>
