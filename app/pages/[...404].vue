932fe5de6760edc04e814ca47a23fd8daea46027
betogroo <luizhumberto@gmail.com>
2026-01-02 15:09:45 -0300

    refactor: move empty-state to own component

--- app/pages/[...404].vue
+++ app/pages/[...404].vue
@@ -9,21 +9,9 @@
 
 <template>
   <app-public-container>
-    <v-empty-state :image="imgSrc" size="300" text-width="350">
-      <template #media>
-        <v-img class="mb-8" />
-      </template>
-
-      <template #title>
-        <div class="text-h6 text-high-emphasis">Oooops! Algo deu errado</div>
-      </template>
-
-      <template #text>
-        <div class="text-body-1 text-justify">
-          A chave se quebrou e a página não foi encontrada.
-          <nuxt-link to="/">clique para retornar a página inicial</nuxt-link>
-        </div>
-      </template>
-    </v-empty-state>
-  </app-public-container>
+    <ui-empty-state
+      :img-src="imgSrc"
+      text="A chave se quebrou e a página não foi encontrada."
+      title="Oooops! Algo deu errado"
+  /></app-public-container>
 </template>
