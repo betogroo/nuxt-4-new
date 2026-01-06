b15a4bf0b23c79443cef3b1e9982254c67de54f0
betogroo <luizhumberto@gmail.com>
2026-01-06 15:06:44 -0300

    feat: create ui-link-back component

---
+++ app/components/ui/link/back.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+  const router = useRouter()
+  const goBack = () => router.back()
+</script>
+
+<template>
+  <ui-btn variant="text" @click="goBack">Voltar</ui-btn>
+</template>
