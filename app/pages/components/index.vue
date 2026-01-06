2af4df43742e53ca50042fffb556ec25c50d46e3
betogroo <luizhumberto@gmail.com>
2026-01-06 15:38:29 -0300

    feat: create components index page

---
+++ app/pages/components/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  definePageMeta({
+    layout: 'default',
+    menu: {
+      title: 'Componentes',
+      hidden: false,
+      icon: 'plus',
+      areas: ['navbar'],
+    },
+  })
+
+  const { getMenuByArea } = useNavigation()
+  const componentsPages = getMenuByArea('components')
+</script>
+
+<template>
+  <div>
+    <ui-heading :level="3">Componentes</ui-heading>
+    <ul>
+      <li v-for="item in componentsPages" :key="item.path">
+        <nuxt-link :to="item.path">{{ item.menu?.title }}</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
