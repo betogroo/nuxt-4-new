55950472bc8e6af6aaf2871a1c92dc4b53217759
betogroo <luizhumberto@gmail.com>
2026-01-06 15:39:37 -0300

    feat: create dialog and notification page

---
+++ app/pages/components/dialog.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  definePageMeta({
+    layout: 'default',
+    menu: {
+      title: 'Ui Dialog',
+      hidden: false,
+      areas: ['components'],
+      priority: 'dialog',
+    },
+  })
+</script>
+
+<template>
+  <div>
+    <ui-heading>Dialog</ui-heading>
+    <ui-link-back />
+  </div>
+</template>
---
+++ app/pages/components/notification.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  definePageMeta({
+    layout: 'default',
+    menu: {
+      title: 'Ui Notifications',
+      hidden: false,
+      areas: ['components'],
+      priority: 'notifications',
+    },
+  })
+</script>
+
+<template>
+  <div>
+    <ui-heading>Notification</ui-heading>
+    <ui-link-back />
+  </div>
+</template>
