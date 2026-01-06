109f872cdf65ac32e4fcfedb670617d13504a745
betogroo <luizhumberto@gmail.com>
2026-01-06 15:12:04 -0300

    feat: add fallback

--- app/components/ui/link/back.vue
+++ app/components/ui/link/back.vue
@@ -1,6 +1,17 @@
 <script setup lang="ts">
+  interface Props {
+    fallback?: string
+  }
+  const { fallback = '/' } = defineProps<Props>()
+
   const router = useRouter()
-  const goBack = () => router.back()
+  const goBack = () => {
+    if (window.history.length > 1) {
+      router.back()
+    } else {
+      router.push(fallback ?? '/')
+    }
+  }
 </script>
 
 <template>
