e163984c72f0f08dca154b208ce01f038238d1e9
betogroo <luizhumberto@gmail.com>
2026-01-06 11:11:14 -0300

    refactor(ui-btn-icon): set to optional and provide @click option

--- app/components/ui/btn-icon.vue
+++ app/components/ui/btn-icon.vue
@@ -5,12 +5,25 @@
   interface Props {
     icon?: Icon
     variant?: BtnIconVariant
-    to: RouteLocationAsPathGeneric | RouteLocationAsRelativeGeneric | string
+    to?: RouteLocationAsPathGeneric | RouteLocationAsRelativeGeneric | string
     ariaLabel?: string
   }
-  const { variant = 'plain', icon = 'home', ariaLabel = 'Link' } = defineProps<Props>()
+  const { variant = 'plain', icon = 'home', ariaLabel = 'Link', to = '/' } = defineProps<Props>()
+
+  const $emit = defineEmits<{
+    (e: 'click', event: MouseEvent): void
+  }>()
+
+  const onClick = (event: MouseEvent) => $emit('click', event)
 </script>
 
 <template>
-  <v-btn :aria-label="ariaLabel" :icon="ICONS[icon]" :ripple="false" :to="to" :variant="variant" />
+  <v-btn
+    :aria-label="ariaLabel"
+    :icon="ICONS[icon]"
+    :ripple="false"
+    :to="to"
+    :variant="variant"
+    @click="onClick"
+  />
 </template>
