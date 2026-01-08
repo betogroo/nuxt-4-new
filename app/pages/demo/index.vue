b8795509c83c4d77f504c8f9c3c459f11776d7a5
betogroo <luizhumberto@gmail.com>
2026-01-08 16:21:10 -0300

    refactor: move demostration  compontnes to demo folder, and create demo index

---
+++ app/pages/demo/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <ui-heading>Demonstração</ui-heading>
+  <ul>
+    <li>
+      <nuxt-link to="./demo/components">Componentes</nuxt-link>
+    </li>
+  </ul>
+</template>
