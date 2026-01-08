d4174d7b803fd55125688f9d4362560d38cb35c9
betogroo <luizhumberto@gmail.com>
2026-01-08 16:59:16 -0300

    refactor: rdemove demo components from about page

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -11,13 +11,9 @@
   })
 
   const testStore = useTestStore()
-  const notificationStore = useNotificationStore()
 </script>
 <template>
   <div>
     <h1>{{ testStore.upperTitle }} - About</h1>
-    <ui-btn @click="notificationStore.open('Abrindo a partir do about', 'success')"
-      >Abrir Notificação</ui-btn
-    >
   </div>
 </template>
