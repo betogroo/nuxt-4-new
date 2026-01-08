9146778aac13f2d3c9880b4b6655fbb5a2743124
betogroo <luizhumberto@gmail.com>
2026-01-08 11:39:07 -0300

    feat: create dinamic demo component page

--- app/pages/components/[slug].vue
+++ app/pages/components/[slug].vue
@@ -17,12 +17,35 @@
     throw createError({ statusCode: 404 })
   }
 
-  console.log(ui.value)
+  const { notify } = useNotification()
 </script>
 
 <template>
-  <article v-if="ui">
-    <ContentRenderer v-if="ui" :value="ui" />
-    <ui-link-back />
-  </article>
+  <div v-if="ui">
+    <ClientOnly>
+      <article class="md-content">
+        <ContentRenderer :value="ui" />
+      </article>
+      <div v-if="route.params.slug === 'notification'">
+        <ui-card-grid>
+          <ui-btn color="error" @click="notify('Erro ao realizar a ação', 'error')">Erro</ui-btn>
+          <ui-btn color="success" @click="notify('Ação concluída com êxito', 'success')"
+            >Sucesso</ui-btn
+          >
+          <ui-btn color="warning" @click="notify('Cuidado ao continuar', 'warning')"
+            >Atenção</ui-btn
+          >
+          <ui-btn color="info" @click="notify('O sistema já está operante', 'info')"
+            >Informação</ui-btn
+          >
+          <ui-btn
+            color="warning"
+            @click="notify('O sistema já está operante', 'warning', { timeout: 5000 })"
+            >Informação (5000ms)</ui-btn
+          >
+        </ui-card-grid>
+      </div>
+      <ui-link-back />
+    </ClientOnly>
+  </div>
 </template>
