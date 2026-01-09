65571f8077778c7f5226e3ef72b1785c51832507
betogroo <luizhumberto@gmail.com>
2026-01-09 11:47:46 -0300

    feat: create demo-block

---
+++ app/components/ui/demo-block.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  const isVisible = ref(false)
+  const toggleCode = () => (isVisible.value = !isVisible.value)
+</script>
+
+<template>
+  <ClientOnly>
+    <ui-card variant="outlined">
+      <h5 class="text-right">
+        Ver código <ui-btn-icon :icon="isVisible ? 'arrow-hide' : 'code'" @click="toggleCode" />
+      </h5>
+      <slot name="demo" />
+      <ui-card v-if="isVisible" variant="text">
+        <slot name="code" />
+      </ui-card>
+    </ui-card>
+  </ClientOnly>
+</template>
