9e5f02cfe7cb4a9ade27dde8c68bd69127bbb862
betogroo <luizhumberto@gmail.com>
2026-01-12 14:05:23 -0300

    feat(wip): create heading  demo

---
+++ app/components/demo/heading.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  import type { HeadingLevel } from '~/types'
+
+  const levels: HeadingLevel[] = [1, 2, 3, 4, 5, 6]
+</script>
+
+<template>
+  <ui-card-grip>
+    <ui-heading v-for="level in levels" :key="level" :level="level">Level {{ level }}</ui-heading>
+  </ui-card-grip>
+</template>
