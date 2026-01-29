7b5d1c1d3e4d30fc93bff05f381becea5b6a62db
betogroo <luizhumberto@gmail.com>
2026-01-29 16:51:34 -0300

    feat: create global error page

---
+++ app/error.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  import type { NuxtError } from '#app'
+  defineProps<{
+    error: NuxtError
+  }>()
+
+  const imgSrc = new URL('~/assets/img/404.png', import.meta.url).href
+</script>
+<template>
+  <app-public-container>
+    <ui-empty-state :img-src="imgSrc" :text="error.message" :title="`Erro ${error.statusCode}`" />
+  </app-public-container>
+</template>
