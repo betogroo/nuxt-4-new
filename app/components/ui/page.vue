319daca7f98abd0c83aa5db1a2e284cc402d4a16
betogroo <luizhumberto@gmail.com>
2026-01-30 16:12:27 -0300

    feat: create default page structure

---
+++ app/components/ui/page.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  interface Props {
+    title?: string
+    showBack?: boolean
+  }
+  const { title = '' } = defineProps<Props>()
+</script>
+
+<template>
+  <section>
+    <header v-if="showBack || title || $slots.title" class="d-flex align-center justify-start ga-2">
+      <div v-if="showBack" class="text-left"><ui-link-back icon /></div>
+      <ui-heading v-if="title" :level="3">{{ title }}</ui-heading>
+      <slot v-else-if="$slots.title" name="title" />
+    </header>
+    <main>
+      <slot />
+    </main>
+    <footer v-if="$slots.footer" class="page-footer mb-6">
+      <slot name="footer" />
+    </footer>
+  </section>
+</template>
