8ce718d2d5dd9908114dd02c954d6d601b7ada30
betogroo <luizhumberto@gmail.com>
2026-02-20 10:43:26 -0300

    refactor: header columns

--- app/components/ui/page.vue
+++ app/components/ui/page.vue
@@ -3,30 +3,44 @@
     title?: string
     showBack?: boolean
   }
-  const { title = '' } = defineProps<Props>()
+  const { title = '', showBack = false } = defineProps<Props>()
+  const slots = useSlots()
+
+  const hasHeader = showBack || !!title || !!slots.title || !!slots.header_action
 </script>
 
 <template>
   <section>
     <header>
       <slot v-if="$slots.breadcrumb" name="breadcrumb" />
-      <div v-if="showBack || title || $slots.title" class="d-flex align-center justify-start ga-2">
-        <div v-if="showBack" class="text-left"><ui-link-back icon /></div>
+
+      <div v-if="hasHeader" class="d-flex align-center justify-start ga-2">
+        <div v-if="showBack" class="text-left">
+          <ui-link-back icon />
+        </div>
+
         <v-row align="center">
-          <v-col cols="6">
-            <ui-heading v-if="title" :level="3">{{ title }}</ui-heading>
+          <!-- COLUNA ESQUERDA -->
+          <v-col>
+            <ui-heading v-if="title" :level="3">
+              {{ title }}
+            </ui-heading>
+
             <slot v-else-if="$slots.title" name="title" />
           </v-col>
 
-          <v-col v-if="$slots.header_action" class="text-right" cols="6"
-            ><slot name="header_action"
-          /></v-col>
+          <!-- COLUNA DIREITA (sempre posição 2) -->
+          <v-col v-if="$slots.header_action" class="text-right" cols="auto">
+            <slot name="header_action" />
+          </v-col>
         </v-row>
       </div>
     </header>
+
     <main>
       <slot />
     </main>
+
     <footer v-if="$slots.footer" class="page-footer mb-6">
       <slot name="footer" />
     </footer>
