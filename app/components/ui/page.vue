0ea2806442dbcb1be233e85bff05df893cdbc64f
betogroo <luizhumberto@gmail.com>
2026-02-20 10:23:39 -0300

    feat(ui-page): add header_action slot

--- app/components/ui/page.vue
+++ app/components/ui/page.vue
@@ -12,8 +12,16 @@
       <slot v-if="$slots.breadcrumb" name="breadcrumb" />
       <div v-if="showBack || title || $slots.title" class="d-flex align-center justify-start ga-2">
         <div v-if="showBack" class="text-left"><ui-link-back icon /></div>
-        <ui-heading v-if="title" :level="3">{{ title }}</ui-heading>
-        <slot v-else-if="$slots.title" name="title" />
+        <v-row align="center">
+          <v-col cols="6">
+            <ui-heading v-if="title" :level="3">{{ title }}</ui-heading>
+            <slot v-else-if="$slots.title" name="title" />
+          </v-col>
+
+          <v-col v-if="$slots.header_action" class="text-right" cols="6"
+            ><slot name="header_action"
+          /></v-col>
+        </v-row>
       </div>
     </header>
     <main>
