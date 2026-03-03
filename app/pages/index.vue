1f36fbf825c7c79585edece8ec7c02547577c641
betogroo <luizhumberto@gmail.com>
2026-03-03 08:40:18 -0300

    feat: load current profile

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+  import type { Profile } from '~/types'
+
   definePageMeta({
     layout: 'default',
     menu: {
@@ -16,12 +18,24 @@
   const tempTitle = ref(title.value)
 
   watch(title, (v) => (tempTitle.value = v))
+
+  const supabase = useSupabaseClient()
+  const profiles = ref<Profile[] | null>([])
+  try {
+    const { data } = await supabase.from('profiles').select('*')
+    if (data) profiles.value = data
+  } catch (error) {
+    console.log(error)
+  }
 </script>
-!
+
 <template>
   <ui-page :title="`${upperTitle} - Home page`">
     <ui-text-field v-model="tempTitle" label="Teste" name="test" />
     <ui-btn @click="testStore.setTitle(tempTitle)">Alterar</ui-btn>
     <ui-btn color="red" variant="outlined" @click="testStore.$reset">Reset</ui-btn>
+    <pre>
+      {{ profiles }}
+    </pre>
   </ui-page>
 </template>
