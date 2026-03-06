e39288c7148319b6f64c5bf0e68e9bce55756b3b
betogroo <luizhumberto@gmail.com>
2026-03-06 13:43:54 -0300

    fix: param name

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -30,7 +30,7 @@
     <ui-text-field v-model="tempTitle" label="Teste" name="test" />
     <ui-btn @click="testStore.setTitle(tempTitle)">Alterar</ui-btn>
     <ui-btn color="red" variant="outlined" @click="testStore.$reset">Reset</ui-btn>
-    <ui-list v-if="units?.length" items="units" :status="status">
+    <ui-list v-if="units?.length" :items="units" :status="status">
       <ui-list-item v-for="item in units" :key="item.id">{{ item.name }}</ui-list-item>
     </ui-list>
     <pre>{{ units }}</pre>
