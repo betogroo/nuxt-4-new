2a0046be56445798bf00739d4f414b13ad1cf874
betogroo <luizhumberto@gmail.com>
2026-03-09 07:15:03 -0300

    text: move data to ui-select

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -30,10 +30,7 @@
     <ui-text-field v-model="tempTitle" label="Teste" name="test" />
     <ui-btn @click="testStore.setTitle(tempTitle)">Alterar</ui-btn>
     <ui-btn color="red" variant="outlined" @click="testStore.$reset">Reset</ui-btn>
-    <ui-list v-if="units?.length" :items="units" :status="status">
-      <ui-list-item v-for="item in units" :key="item.id">{{ item.name }}</ui-list-item>
-    </ui-list>
-    <pre>{{ units }}</pre>
+    <ui-select item-subtitle="name_bec" item-title="name" :items="units || []" name="units" />
     <div>{{ status }}</div>
   </ui-page>
 </template>
