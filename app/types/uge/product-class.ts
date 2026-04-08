6c1e30ceb56aa2f008d75240614cc4cc823c1a69
betogroo <luizhumberto@gmail.com>
2026-04-08 08:20:55 -0300

    chore(porduct_class): add aditional types

--- app/types/uge/product-class.ts
+++ app/types/uge/product-class.ts
@@ -1,4 +1,10 @@
 import type { z } from '~/schemas'
 import type { ProductClassSchema } from '~/schemas/uge/entities/product-class.schema'
+import type {
+  ProductClassReadSchema,
+  ProductClassReadSelectSchema,
+} from '~/schemas/uge/dto/product-class.read.dto'
 
 export type ProductClass = z.infer<typeof ProductClassSchema>
+export type ProductClassRead = z.infer<typeof ProductClassReadSchema>
+export type ProductClassReadSelect = z.infer<typeof ProductClassReadSelectSchema>
