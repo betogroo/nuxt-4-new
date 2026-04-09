c0791f4ebb84cabf2f3112ceb574934882a6d2ad
betogroo <luizhumberto@gmail.com>
2026-04-09 08:48:58 -0300

    refactor(UseGetTable): add View type

--- app/composables/useTableGet.ts
+++ app/composables/useTableGet.ts
@@ -1,9 +1,9 @@
 import type { ZodSchema } from 'zod'
 import { AppError } from '~/error/AppError'
-import type { TableName } from '~/types'
+import type { TableName, ViewName } from '~/types'
 
 type UseTableGetOptions<T> = {
-  table: TableName
+  table: TableName | ViewName
   schema: ZodSchema<T>
   select?: string
 }
@@ -14,7 +14,11 @@ const useTableGet = <T>({ table, schema, select = '*' }: UseTableGetOptions<T>) 
     if (import.meta.dev) {
       await delay(DELAY)
     }
-    const { data, error } = await supabase.from(table).select(select).eq('id', key).single()
+    const { data, error } = await supabase
+      .from(table as never)
+      .select(select)
+      .eq('id', key)
+      .single()
     if (error) {
       throw new AppError(`Não foi possível carregar dados de "${table}"`, error)
     }
