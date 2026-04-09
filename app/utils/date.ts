18ea66d579fdfa6ed61d66b8fb0c62585792270e
betogroo <luizhumberto@gmail.com>
2026-04-09 08:48:23 -0300

    fix(date): error UTC

--- app/utils/date.ts
+++ app/utils/date.ts
@@ -1,7 +1,7 @@
 export const dateBr = (value: string | null) => {
   if (!value) return 'Data não cadastrada'
 
-  const date = new Date(value)
+  const date = new Date(value + 'T00:00:00')
 
   if (isNaN(date.getTime())) return 'Data inválida'
 
