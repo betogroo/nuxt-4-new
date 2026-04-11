86eb9e974282e5d36243d2f88c95422ad490421b
betogroo <luizhumberto@gmail.com>
2026-04-11 09:41:39 -0300

    refactor(data.ts): add validations

--- app/utils/date.ts
+++ app/utils/date.ts
@@ -1,7 +1,12 @@
 export const dateBr = (value: string | null) => {
   if (!value) return 'Data não cadastrada'
 
-  const date = new Date(value + 'T00:00:00')
+  let date = new Date(value)
+
+  // Se falhar e for formato date puro, tenta forçar meia-noite
+  if (isNaN(date.getTime()) && /^\d{4}-\d{2}-\d{2}$/.test(value)) {
+    date = new Date(`${value}T00:00:00`)
+  }
 
   if (isNaN(date.getTime())) return 'Data inválida'
 
