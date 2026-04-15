dad0ae7381677ad497d3f1ba2eed8f2aa6ba69a9
betogroo <luizhumberto@gmail.com>
2026-04-15 10:15:48 -0300

    chore: add demands seed

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1546,3 +1546,48 @@ VALUES
         '11ef7c98-b24d-4867-ab60-518ac8d95da3',
         'd641410a-ec55-4567-9458-ce49f516ca98'
     );
+
+INSERT INTO
+    "public"."demands" (
+        "id",
+        "created_at",
+        "updated_at",
+        "description",
+        "electronic_process_number",
+        "internal_process_number",
+        "year",
+        "object_types_id",
+        "owner_id",
+        "dispute_date",
+        "active",
+        "deleted_at"
+    )
+VALUES
+    (
+        '771b2d38-ab7a-4c17-8edc-ccc9061e131c',
+        '2026-04-15 13:14:19.105791+00',
+        '2026-04-15 13:14:19.105791+00',
+        'Gêneros Alimentícios',
+        '11111111/11',
+        '1',
+        '2026',
+        '1',
+        '0c5673ee-43cd-49b6-9606-e1d546484d00',
+        '2026-05-13',
+        'true',
+        null
+    ),
+    (
+        'b1764762-b890-4528-b2d5-60d16c39f428',
+        '2026-04-15 13:14:48.498076+00',
+        '2026-04-15 13:14:48.498076+00',
+        'Material de Escitório e Papelaria',
+        '22222222/22',
+        '2',
+        '2026',
+        '1',
+        '0c5673ee-43cd-49b6-9606-e1d546484d00',
+        '2026-06-11',
+        'true',
+        null
+    );
