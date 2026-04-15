6ca6d0291d0fd9b3d832f93cf180d1dcaa45512c
betogroo <luizhumberto@gmail.com>
2026-04-15 08:57:38 -0300

    temp: clear index page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-  import z from 'zod'
-  import { ProductClassReadSchema, ProductReadDetailsSchema } from '~/schemas'
-
   definePageMeta({
     layout: 'default',
     menu: {
@@ -12,43 +9,8 @@
       priority: 'index',
     },
   })
-
-  const { fetchAll: fetchProducts } = useTableFetch({
-    table: 'products',
-    schema: z.array(ProductReadDetailsSchema),
-    select: `
-      id, name, description,cat_mat, cat_bec,nat_gov,pdm,specifications,active,
-      class:product_class(name, code),
-      expense_type:expense_types(name, expense_number)
-    `,
-  })
-  const { fetchAll: fetchProductClasses } = useTableFetch({
-    table: 'product_class',
-    schema: z.array(ProductClassReadSchema),
-  })
-  const products = await fetchProducts()
-  const classes = await fetchProductClasses()
-
-  //const supabase = useSupabaseClient()
-
-  const { fetchAll } = useTableFetch({
-    table: 'product_details_active',
-    schema: z.array(ProductReadDetailsSchema),
-  })
-
-  const testView = await fetchAll()
 </script>
 
 <template>
-  <ui-page :title="`Home page`">
-    <pre>
-      {{ products }}
-    </pre>
-    <pre>
-      {{ classes }}
-    </pre>
-    <pre>
-      {{ testView }}
-    </pre>
-  </ui-page>
+  <ui-page :title="`Home page`" />
 </template>
