f23ac4341fcaf0d1b49fdec4a44554f99cc7c6d5
betogroo <luizhumberto@gmail.com>
2026-04-19 17:58:47 -0300

    refactor: add try/catch

--- app/composables/useDemandPage.ts
+++ app/composables/useDemandPage.ts
@@ -1,8 +1,20 @@
+import { AppError } from '~/error/AppError'
+
 const useDemandPage = async (id: string) => {
   const { get } = useDemand()
 
   const { data, status, error, pending, refresh } = await useAsyncData(`demand${id}`, () => {
-    return get(id)
+    try {
+      return get(id)
+    } catch (error) {
+      if (error instanceof AppError) {
+        throw createError({ statusCode: 400, message: error.message })
+      }
+      throw createError({
+        statusCode: 500,
+        message: 'Erro inesperado',
+      })
+    }
   })
 
   return { demand: data.value, status, error, pending, refresh }
