423fa1148dfeb7fbecaa6d20142da3cae65f6206
betogroo <luizhumberto@gmail.com>
2026-04-21 20:28:29 -0300

    chore: create Status type and schema

--- app/types/ui/general.ts
+++ app/types/ui/general.ts
@@ -1,4 +1,5 @@
 import type z from 'zod'
-import type { UiDensitySchema } from '~/schemas/ui'
+import type { UiDensitySchema, StatusSchema } from '~/schemas/ui'
 
 export type UiDensity = z.infer<typeof UiDensitySchema>
+export type Status = z.infer<typeof StatusSchema>
