c2dcb2b11bc7e3967d36ed764c7a36c77129b8b3
betogroo <luizhumberto@gmail.com>
2026-04-21 20:26:41 -0300

    fix: error on schema

--- app/schemas/uge/dto/packaging-type.read.dto.ts
+++ app/schemas/uge/dto/packaging-type.read.dto.ts
@@ -3,4 +3,6 @@ import { PackagingTypeSchema } from '../entities/packaging-type.schema'
 export const PackagingTypeReadSchema = PackagingTypeSchema.omit({
   created_at: true,
   updated_at: true,
+  active: true,
+  deleted_at: true,
 })
