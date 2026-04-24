fedd8f88644f33562433a418377bec87b438f722
betogroo <luizhumberto@gmail.com>
2026-04-24 14:12:17 -0300

    feat: add edit product page

---
+++ app/pages/uge/product/[id]/edit.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+  const { params } = useRoute()
+  const id = computed(() => params.id as string)
+</script>
+
+<template>
+  <ui-page title="Edit">{{ id }}</ui-page>
+</template>
