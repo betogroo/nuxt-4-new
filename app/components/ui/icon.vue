4116e2ba6fef0a2a5974c5d653e6dcbc76bbb47a
betogroo <luizhumberto@gmail.com>
2026-07-17 16:26:53 -0300

    feat: create ui-icon component

---
+++ app/components/ui/icon.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  import type { Icon } from '~/types/ui/icons'
+
+  interface Props {
+    icon: Icon
+    color?: string
+  }
+  const { color = undefined } = defineProps<Props>()
+</script>
+
+<template>
+  <v-icon :color="color" v-bind="$attrs" :icon="ICONS[icon]" />
+</template>
